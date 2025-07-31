612fbce0c65def1f1e706feabeec67bfc19862b9
João Henrique <joaokocssis@gmail.com>
2025-07-31 10:48:24 -0300

    adiciona comentário ao código

--- cardapio.py
+++ cardapio.py
@@ -2,6 +2,7 @@
 import wget
 import os
 
+#Apaga arquivos que existam para não sobrecarregar o diretório
 for indice in range(1,6):
     try:
         arquivo = f"cardapio_{indice}.pdf"
@@ -12,9 +13,10 @@ for indice in range(1,6):
         print(f'Erro ao apagar o arquivo: {e}')
 
 
-
+#Arquivo URL com http básico
 URL = "http://ceasacampinas.empresarial.ws/ae/principal/baixa.php"
 for numero in range(1,6):
+    #mescla a url básica com a extensão para cardápios de 1 a 5
     FULL = f"{URL}?file=rcee1%20{numero}&tipo=pdf"
     SAIDA = f"cardapio_{numero}.pdf"
     wget.download(FULL, SAIDA)
